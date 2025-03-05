ca7ea93ead9daea715d269f94176a9ac8454927a
DAQEM <daqemyt@gmail.com>
2025-03-05 19:02:40 +0100

    Added wiki page status migration

---
+++ Projeli.WikiService.Api/Migrations/20250305180227_AddedWikiPageStatus.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projeli.WikiService.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedWikiPageStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPublished",
+                table: "Pages");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Pages",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Pages");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublished",
+                table: "Pages",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
