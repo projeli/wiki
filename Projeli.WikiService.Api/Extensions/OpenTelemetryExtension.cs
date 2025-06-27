a3dec3d73976f4e22efdc56193576befdaf50886
DAQEM <daqemyt@gmail.com>
2025-06-28 00:13:27 +0200

    Excluding /metrics from monitoring.

--- Projeli.WikiService.Api/Extensions/OpenTelemetryExtension.cs
+++ Projeli.WikiService.Api/Extensions/OpenTelemetryExtension.cs
@@ -9,14 +9,15 @@ namespace Projeli.WikiService.Api.Extensions;
 
 public static class OpenTelemetryExtension
 {
-    public static void AddWikiServiceOpenTelemetry(this IServiceCollection services, ILoggingBuilder loggingBuilder, IConfiguration configuration)
+    public static void AddWikiServiceOpenTelemetry(this IServiceCollection services, ILoggingBuilder loggingBuilder,
+        IConfiguration configuration)
     {
         loggingBuilder.AddOpenTelemetry(builder =>
         {
             builder.IncludeScopes = true;
             builder.IncludeFormattedMessage = true;
         });
-        
+
         services.AddOpenTelemetry()
             .ConfigureResource(builder => builder.AddService("wiki-service"))
             .WithMetrics(builder =>
@@ -29,32 +30,36 @@ public static class OpenTelemetryExtension
             .WithTracing(builder =>
             {
                 builder
-                    .AddAspNetCoreInstrumentation()
+                    .AddAspNetCoreInstrumentation(options =>
+                    {
+                        options.Filter = context =>
+                            context.Request.Path.ToString() is not "/metrics" and not "/health" and not "/alive";
+                    })
                     .AddHttpClientInstrumentation()
                     .AddGrpcClientInstrumentation();
             });
-        
+
         var useOtlpExporter = !string.IsNullOrWhiteSpace(configuration["OTEL_EXPORTER_OTLP_ENDPOINT"]);
-        
+
         if (useOtlpExporter)
         {
             services.Configure<OpenTelemetryLoggerOptions>(logging => logging.AddOtlpExporter());
             services.ConfigureOpenTelemetryMeterProvider(metrics => metrics.AddOtlpExporter());
             services.ConfigureOpenTelemetryTracerProvider(tracing => tracing.AddOtlpExporter());
         }
-        
+
         services.AddOpenTelemetry().WithMetrics(x => x.AddPrometheusExporter());
 
         services.AddHealthChecks()
             .AddCheck("self", () => HealthCheckResult.Healthy(), ["live"]);
     }
-    
+
     public static void UseWikiServiceOpenTelemetry(this WebApplication app)
     {
         app.MapPrometheusScrapingEndpoint();
-        
+
         app.MapHealthChecks("/health");
-        
+
         app.MapHealthChecks("/alive", new HealthCheckOptions
         {
             Predicate = r => r.Tags.Contains("live")
